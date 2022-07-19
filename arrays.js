2f5a0164b558b364ce8ed5bca7af64de95aff11b
Christine Gatwiri <christinegatwiri18@gmail.com>
2022-07-20 01:59:44 +0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,5 +1,4 @@
 function solution(A) {
-    // write your code in JavaScript (Node.js 8.9.4)
     // 1. Array range is given as n+1, e.g if n is 5, the range is 6, meaning 1,2,3,4,5,6. But the array misses one of the numbers. find the missing one.
     // 1. loop through all numbers in the range given , store in array, compare two arrays, print missing elemnt
     Array.prototype.difference = function(e) {
